ba855cf00062aee4eaf6cbaf7e60481133487811
woo0218 <jungwoo9093@gmail.com>
2026-03-18 17:29:47 +0900

    할일 수정

--- mission todoapp/src/App.jsx
+++ mission todoapp/src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState(4);
+  const [todoId, setTodoId] = useState(4);
 
-  const [todoId, setTodoId] = useState([
-    {id:3, text:'공부하기', checked:false},
-    {id:2, text:'코딩하기', checked:false},
-    {id:1, text:'운동하기', checked:false},
-
+  const [todos, setTodos] = useState([
+    { id: 3, text: "공부하기", checked: false },
+    { id: 2, text: "코딩하기", checked: false },
+    { id: 1, text: "운동하기", checked: false },
   ]);
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    setTodos([{ text: form.todo.value, checked: false }, ...todos]);
+    setTodos([{ id: todoId, text: form.todo.value, checked: false }, ...todos]);
+    setTodoId(todoId + 1);
   };
 
-  const removeTodo = (selectedIndex) => {
-    const filterTodos = todos.filter((todo, index) => index != selectedIndex);
+  const removeTodo = (seletedId) => {
+    const filterTodos = todos.filter((todo) => todo.id != seletedId);
     setTodos(filterTodos);
+  };
+
+  const toggleTodo = (seletedId) => {
+    const updateTodos = todos.map((todo) =>
+      todo.id == seletedId ? { ...todo, checked: !todo.checked } : todo,
+    );
+    setTodos(updateTodos);
   };
 
   return (
@@ -28,11 +35,16 @@ function App() {
         <button type="submit">등록</button>
       </form>
       <ul>
-        {todos.map((todo, i) => (
-          <li key={i}>
-            <input type="checkbox" />
-            {i} / {todo.text}
-            <button onClick={() => removeTodo(i)}>X</button>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              onChange={() => {
+                toggleTodo(todo.id);
+              }}
+            />
+            {JSON.stringify(todo.checked)} / {todo.id} / {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>X</button>
           </li>
         ))}
       </ul>
